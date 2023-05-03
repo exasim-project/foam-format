c0461779c550136c1bd3fb5e6e115b268e9cfaba
Gregor Olenik <gregor.olenik@web.de>
2023-05-03 09:28:00 +0200

    new package format

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # foam-format
+
+## Usage 
+Install with pip install . and run foam-format <file_or_folder>
+
+## Caveats
+Currently header files are not automatically formated
---
+++ src/foam_format/cli.py
@@ -0,0 +1,36 @@
+from .core import format_body, is_header_file
+from pathlib import Path
+import os
+import logging
+import sys
+from tqdm import tqdm
+
+
+def main():
+    fn = Path(sys.argv[1])
+    skip_if_orig_exists = False
+    logging.basicConfig(format="[FOAM-FORMAT] %(message)s", level=20)
+    if fn.is_dir():
+        file_list = []
+        logging.info("collecting files")
+        for r, ds, fs in os.walk(fn):
+            # dont descent into Make folders
+            exclude = ["Make", "lnInclude"]
+            ds[:] = [d for d in ds if d not in exclude]
+            for f in fs:
+                # NOTE currently header files are not formated
+                if is_header_file(r, f):
+                    continue
+                # if ".orig" in f:
+                #    continue
+                # if skip_if_orig_exists and (Path(r) / f"{f}.orig").exists():
+                #    continue
+                file_list.append(Path(r) / f)
+    else:
+        file_list = [Path(fn)]
+
+    logging.info(f"formating")
+    lines = 0
+    for f in tqdm(file_list):
+        lines += format_body(f)
+    logging.info(f"reformated {lines} lines")
